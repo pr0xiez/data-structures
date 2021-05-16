fc8d9241fc7268fc870dbdbefba40be678304785
Alex Hall <ahall@cincom.com>
2021-05-16 19:43:26 -0400

    demo

--- linked-lists/app.js
+++ linked-lists/app.js
@@ -10,10 +10,14 @@ const list = LinkedList()
 list.insertAtHead(Cats[0])
 list.insertAtHead(Cats[1])
 list.insertAtHead(Cats[2])
+console.log(list.getByIndex(1).value)
 
-// console.log(list)
+list.insertAtIndex(3, { name: 'Tom', id: '4' })
+console.log(list.getByIndex(3).value)
 
-// console.log(list.getByIndex(1).value)
+list.removeAtIndex(3)
+console.log(list.getByIndex(3))
 
-const ll = LinkedList.fromValues(10, 20)
-console.log(ll.getByIndex(0))
+console.log(list.getByIndex(0).value)
+list.removeHead()
+console.log(list.getByIndex(0).value)
